50f7ff1515fc2359c5dd3787cc06f04455450996
Amit Varghese <amitvarghese2011@gmail.com>
2020-02-05 01:31:54 +0530

    Updates

---
+++ precache-manifest.877c746a0fb26cfab3d2db17a5c1018d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6347b216828336fd62bdf2a40fbc1ba2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "df74c4a6da18f3100f50",
+    "url": "/robofriends/static/css/main.39575be1.chunk.css"
+  },
+  {
+    "revision": "75fa1c9155c400242a6c",
+    "url": "/robofriends/static/js/2.a22a475e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.a22a475e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df74c4a6da18f3100f50",
+    "url": "/robofriends/static/js/main.504627ca.chunk.js"
+  },
+  {
+    "revision": "1e6fdd492adfb4acd267",
+    "url": "/robofriends/static/js/runtime-main.2c4b189a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robofriends/static/media/logo.5d5d9eef.svg"
+  }
+]);
